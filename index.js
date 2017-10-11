f0b07e8e570dce4355f2d2028de32f980c9502fa
Daniel Pintilei <daniel.pintilei@assist.ro>
2017-10-11 11:45:04 +0300

    Add icons

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const apiController = require('./server/controllers/apiController')
 
 const port = process.env.PORT || 3300
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+if (!process.env.NOW) {
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
+}
 
 app.use(express.static('build'))
 
